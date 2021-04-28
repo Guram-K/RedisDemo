3f525392024ea9d4bbc6fbd313da315af795f267
Guram Kasradze <g.kasradze@sweeftdigital.com>
2021-04-28 16:50:56 +0400

    Add project files.

---
+++ RedisDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using RedisDemo.Extensions;
+using System.Threading.Tasks;
+
+namespace RedisDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IDistributedCache _cache;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDistributedCache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        [HttpGet("/GetWeather")]
+        public async Task<ActionResult<WeatherForecast>> Get()
+        {
+            return Ok(await _cache.GetRecordAsync<WeatherForecast>("12"));
+        }
+
+        [HttpPost("/PostWeather")]
+        public async Task<ActionResult> Post([FromQuery]WeatherForecast foreCast)
+        {
+            await _cache.SetRecordAsync<WeatherForecast>("12", foreCast);
+
+            return Ok();
+        }
+    }
+}
---
+++ RedisDemo/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RedisDemo.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+            string recordId,
+            T data,
+            TimeSpan? absoluteExpireTime = null,
+            TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            //expire time for the value;
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
+
+            //expire value even if AbsoluteExpiration is not passed, if the value is not being accessed in SlidingExpiration time;
+            options.SlidingExpiration = unusedExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            //Get json from cache;
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if (jsonData is null)
+                return default(T);
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
